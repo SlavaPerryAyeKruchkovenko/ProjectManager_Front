f166e1c3423bd34b391038e1237d48d53e91c8fa
Slava <sh-e-95@mail.ru>
2022-02-27 18:07:15 +0500

    create header

--- project-manager/src/app/main-template/header/header.component.ts
+++ project-manager/src/app/main-template/header/header.component.ts
@@ -1,12 +1,49 @@
-import {Component} from "@angular/core";
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from "@angular/core";
+import { fromEvent, Observable, Subscription } from "rxjs";
 
 @Component({
   selector:'app-header',
-  styleUrls:['./header.component.sass'],
+  styleUrls:['./header.component.sass','../main-template.component.sass'],
   templateUrl:'./header.component.html',
   })
 
-export class HeaderComponent {
+
+export class HeaderComponent implements OnDestroy,OnInit,AfterViewInit{
+
   imgLink = "#";
   mainImg = "../../../assets/images/logo.png"
+  signIn = "#";
+  signUp = "#"
+  private minWidth = 768;
+  private resizeObservable: Observable<Event> = fromEvent(window, 'resize')
+  private resizeSubscription: Subscription = new Subscription()
+  @ViewChild('signIn') signInObj: ElementRef | undefined;
+
+  private changeSignMenu(signInObj:ElementRef,minWidth:number){
+    if(signInObj != undefined){
+      const signBlock = signInObj.nativeElement;
+      if(window.innerWidth < minWidth)
+        signBlock.classList.add("hide");
+
+      else if(signBlock.classList.contains("hide"))
+        signBlock.classList.remove("hide");
+    }
+  }
+  ngOnInit() {
+    this.resizeSubscription = this.resizeObservable.subscribe( evt => {
+      if(this.signInObj != undefined)
+        this.changeSignMenu(this.signInObj,this.minWidth)
+    })
+  }
+  ngOnDestroy() {
+    this.resizeSubscription.unsubscribe()
+  }
+
+  ngAfterViewInit(): void {
+    if(this.signInObj != undefined)
+      this.changeSignMenu(this.signInObj,this.minWidth)
+  }
 }
+
+
+
