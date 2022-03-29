22d71788da9c2e7c9dff567a4c8d3539a5d985f7
Slava <sh-e-95@mail.ru>
2022-03-29 19:58:22 +0500

    unwork header menu

--- project-manager/src/app/main-template/main-template.component.ts
+++ project-manager/src/app/main-template/main-template.component.ts
@@ -1,28 +1,43 @@
-import {Component, EventEmitter, HostListener, OnInit} from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  EventEmitter, Inject,
+  OnInit,
+} from '@angular/core';
 import Size from "../../models/Size";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-main-template',
   templateUrl: './main-template.component.html',
   styleUrls: ['./main-template.component.sass']
 })
-export class MainTemplateComponent implements OnInit {
+export class MainTemplateComponent implements OnInit{
 
+  private _scrollPosition: number = 0;
   private _menuIsOpen: boolean = false;
   private _headerSize: Size = new Size(0, 0);
+
+  @Inject(ChangeDetectorRef)changeDetection: ChangeDetectorRef | undefined;
+
+  onContentScrolled(e: Event) {
+
+    if (window.scrollY - this._scrollPosition < 0 && !this.isHeaderVisible) {
+      this.isHeaderVisible = true;
+      console.log(this.changeDetection);
+      this.changeDetection?.markForCheck();
+    } else if(window.scrollY - this._scrollPosition > 0 && this.isHeaderVisible) {
+      this.isHeaderVisible = false;
+      console.log(this.changeDetection);
+      this.changeDetection?.markForCheck();
+    }
+    this._scrollPosition = window.scrollY;
+  }
+
   menuEmitter: EventEmitter<boolean> = new EventEmitter<boolean>();
   headerHeightEmitter: EventEmitter<number> = new EventEmitter<number>();
   headerWidthEmitter: EventEmitter<number> = new EventEmitter<number>();
-
-  @HostListener('scroll', ['$event.target'])
-  onContentScrolled(e: HTMLElement) {
-    let scroll = e.scrollTop;
-    if (e.scrollTop > 0) {
-      console.log("scrollDown");
-    } else {
-      console.log("scrollUp");
-    }
-  }
+  isHeaderVisible = true;
   get menuIsOpen(): boolean {
     return this._menuIsOpen
   }
@@ -31,15 +46,15 @@ export class MainTemplateComponent implements OnInit {
     return this._headerSize
   }
 
-  private set headerSize(value) {
+  set headerSize(value) {
     if (value.height >= 0 && value.height >= 0)
       this._headerSize = value;
   }
 
   ngOnInit(): void {
-    //window.addEventListener('scroll', this.onContentScrolled, true);
+    window.addEventListener('scroll', this.onContentScrolled,false);
     this.menuEmitter.subscribe(value => this._menuIsOpen = value);
-    this.headerHeightEmitter.subscribe(value => this.headerSize = new Size(this._headerSize.width, value));
-    this.headerWidthEmitter.subscribe(value => this.headerSize = new Size(this._headerSize.width, value));
+    this.headerHeightEmitter.subscribe(value => this._headerSize = new Size(this._headerSize.width, value));
+    this.headerWidthEmitter.subscribe(value => this._headerSize = new Size(this._headerSize.width, value));
   }
 }
